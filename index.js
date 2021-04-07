c69f1976968efeaca33122ad22c971a16848730b
Josh <leonidus00@yahoo.com>
2021-04-07 14:57:59 -0500

    commit tree 1a5a779b42d76a325b2f9f0c61dd4d3650e7a673

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = (num1 * num2)
+let random = (1)
+const num3 = 12
+const num4 = 3
+let mod = (num3 / num4)
+let num5 = 20
+Math.max(num1, num5)
+let max = (num5)
